13126878c67ae5234b009720886862e22d453405
Nobojit Barman <nobojitbarmon@gmail.com>
2026-08-13 12:55:06 +0600

    property model - property routes - server js  has been written only controller is left here

---
+++ backend/src/controllers/PropertiesController.js
@@ -0,0 +1,19 @@
+const prisma = require("../config/prisma");
+
+
+const properties= (req, res) => {
+
+    try {
+         const {name ,   address,  total_units} = req.body
+        
+    } catch (error) {
+        
+    }
+
+
+
+
+
+
+
+}
